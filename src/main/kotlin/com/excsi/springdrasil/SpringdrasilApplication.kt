79cf9b090196478802fe75152c52316414a5fc61
Excsi <1aastreaa@gmail.com>
2026-08-26 21:46:32 -0400

    Initial commit

---
+++ src/main/kotlin/com/excsi/springdrasil/SpringdrasilApplication.kt
@@ -0,0 +1,11 @@
+package com.excsi.springdrasil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdrasilApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringdrasilApplication>(*args)
+}
---
+++ src/test/kotlin/com/excsi/springdrasil/SpringdrasilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.excsi.springdrasil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdrasilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
