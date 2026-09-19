7a7887060b4b4fe1fab9b9fb7b5653c246e3ab0d
Excsi <1aastreaa@gmail.com>
2026-09-18 20:55:02 -0400

    Update README.md

--- src/main/kotlin/dev/excsi/springdrasil/controller/authlib/ApiMetadataController.kt
+++ src/main/kotlin/dev/excsi/springdrasil/controller/authlib/ApiMetadataController.kt
@@ -3,6 +3,7 @@ package dev.excsi.springdrasil.controller.authlib
 import dev.excsi.springdrasil.configuration.ConfigurationValues
 import dev.excsi.springdrasil.dto.ApiMetadataResponse
 import dev.excsi.springdrasil.service.YggdrasilSignatureService
+import org.springframework.boot.info.BuildProperties
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class ApiMetadataController(
     val configurationValues: ConfigurationValues,
     val yggdrasilSignatureService: YggdrasilSignatureService,
+    val buildProperties: BuildProperties
 ) {
 
     @GetMapping("/", "")
@@ -20,7 +22,7 @@ class ApiMetadataController(
             meta = mapOf(
                 "serverName" to "SpringdrasilAuthServer",
                 "implementationName" to "Springdrasil",
-                "implementationVersion" to "0.0.1",
+                "implementationVersion" to "${buildProperties.version}",
                 "feature.non_email_login" to false,
                 "links" to mapOf(
                     "homepage" to configurationValues.baseDomainUrl,
